a0ad188071621d7bdfd046dc26ee1f824a981f24
dvijtest <dvijtest@gmail.com>
2023-11-17 16:23:35 +0530

    dsc-ghostmode

---
+++ README.md
@@ -0,0 +1,6 @@
+Discourse ghostmode
+===================
+
+A [plugin] to make a marked posts invisible to everybody else.
+
+[plugin]: https://meta.discourse.org/t/install-a-plugin/19157
---
+++ plugin.rb
@@ -0,0 +1,71 @@
+# name: ghostmode
+# about: Hide a user's posts from everybody else
+# version: 0.0.1
+# authors: Michael Howell (notriddle)
+# url: https://github.com/dvijtest/discourse-ghostmode
+enabled_site_setting :ghostmode_enabled
+
+after_initialize do
+
+  module ::DiscourseShadowbanTopicView
+    def filter_post_types(posts)
+      result = super(posts)
+      if SiteSetting.ghostmode_show_to_staff && @user&.staff?
+        result
+      else
+        result.where(
+          'posts.id NOT IN (?) AND u.id != ?',
+          SiteSetting.ghostmode_posts.split('|'),
+          @user&.id || 0
+        )
+      end
+    end
+  end
+
+  class ::TopicView
+    prepend ::DiscourseShadowbanTopicView
+  end
+
+  module ::DiscourseShadowbanTopicQuery
+    def default_results(options = {})
+      result = super(options)
+      if SiteSetting.ghostmode_show_to_staff && @user&.staff?
+        result
+      else
+        result.where(
+          'topics.id NOT IN (?) AND u.id != ?',
+          SiteSetting.ghostmode_topics.split('|'),
+          @user&.id || 0
+        )
+      end
+    end
+  end
+
+  class ::TopicQuery
+    prepend ::DiscourseShadowbanTopicQuery
+  end
+
+  module ::DiscourseShadowbanPostAlerter
+    def create_notification(user, type, post, opts = {})
+      if (SiteSetting.ghostmode_show_to_staff && user&.staff?) || SiteSetting.ghostmode_posts.split('|').find_index(post.id).nil?
+        super(user, type, post, opts)
+      end
+    end
+  end
+
+  class ::PostAlerter
+    prepend ::DiscourseShadowbanPostAlerter
+  end
+
+  module ::DiscourseShadowbanPostCreator
+    def update_topic_stats
+      if SiteSetting.ghostmode_topics.split('|').find_index(@post.id).nil?
+        super
+      end
+    end
+  end
+
+  class ::PostCreator
+    prepend ::DiscourseShadowbanPostCreator
+  end
+end
