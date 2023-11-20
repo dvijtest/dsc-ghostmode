bd4216428e14e37641e7017f625f23a8a93b7b8a
dvijtest <dvijtest@gmail.com>
2023-11-20 14:09:18 +0530

    dsc-ghostmode 0.0.6

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: ghostmode
 # about: Hide a user's posts from everybody else
-# version: 0.0.5
+# version: 0.0.6
 # authors: dvij
 enabled_site_setting :ghostmode_enabled
 
@@ -11,12 +11,13 @@ after_initialize do
       if SiteSetting.ghostmode_show_to_staff && @user&.staff?
         result
       else
-       # result.where(
-       #  'posts.id NOT IN (?)',
-       #   SiteSetting.ghostmode_posts.split('|')
-       # )
-       result.where(
-          '(posts.id NOT IN (?) OR posts.user_id = ?)',
+        # result.where(
+        #  'posts.id NOT IN (?)',
+        #   SiteSetting.ghostmode_posts.split('|')
+        # )
+        result.where(
+          #'(posts.id NOT IN (?) OR posts.user_id = ?)',
+          '(posts.id NOT IN (?) OR posts.user_id IN (SELECT u.id FROM users u WHERE admin = "t" OR u.id = ?))',
           SiteSetting.ghostmode_posts.split('|'),
           @user&.id || 0
         )
@@ -34,12 +35,13 @@ after_initialize do
       if SiteSetting.ghostmode_show_to_staff && @user&.staff?
         result
       else
-      #  result.where(
-      #    'topics.id NOT IN (?)',
-      #    SiteSetting.ghostmode_topics.split('|')
-      #  )
-      result.where(
-          '(topics.id NOT IN (?) OR topics.user_id = ?)',
+        #  result.where(
+        #    'topics.id NOT IN (?)',
+        #    SiteSetting.ghostmode_topics.split('|')
+        #  )
+        result.where(
+          #'(topics.id NOT IN (?) OR topics.user_id = ?)',
+          '(topics.id NOT IN (?) OR topics.user_id IN (SELECT u.id FROM users u WHERE admin = "t" OR u.id = ?))',
           SiteSetting.ghostmode_topics.split('|'),
           @user&.id || 0
         )
