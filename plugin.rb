03f6f301160778c69b4277035af453bb7d457877
dvijtest <dvijtest@gmail.com>
2023-11-20 15:27:08 +0530

    dsc-ghostmode 0.0.8

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: ghostmode
 # about: Hide a user's posts from everybody else
-# version: 0.0.7
+# version: 0.0.8
 # authors: dvij
 enabled_site_setting :ghostmode_enabled
 
@@ -17,7 +17,7 @@ after_initialize do
         # )
         result.where(
           #'(posts.id NOT IN (?) OR posts.user_id = ?)',
-          '(posts.id NOT IN (?) OR posts.user_id IN (SELECT u.id FROM users u WHERE u.admin = ? OR u.id = ?))',
+          '(posts.id NOT IN (?) OR posts.user_id IN (SELECT u.id FROM users u WHERE admin = ? OR u.id = ?))',
           SiteSetting.ghostmode_posts.split('|'),
           "t",
           @user&.id || 0
@@ -42,7 +42,7 @@ after_initialize do
         #  )
         result.where(
           #'(topics.id NOT IN (?) OR topics.user_id = ?)',
-          '(topics.id NOT IN (?) OR topics.user_id IN (SELECT u.id FROM users u WHERE u.admin = ? OR u.id = ?))',
+          '(topics.id NOT IN (?) OR topics.user_id IN (SELECT u.id FROM users u WHERE admin = ? OR u.id = ?))',
           SiteSetting.ghostmode_topics.split('|'),
           "t",
           @user&.id || 0
