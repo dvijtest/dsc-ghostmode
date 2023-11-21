23b18fbb5fae9dbeb94503bcd0e5a3c6608dd749
dvijtest <dvijtest@gmail.com>
2023-11-21 16:28:07 +0530

    dsc-ghostmode 0.0.30

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: dsc-ghostmode
 # about: Hide a marked posts and topics from other users
-# version: 0.0.29
+# version: 0.0.30
 # authors: dvijtest
 # url: https://github.com/dvijtest/dsc-ghostmode
 enabled_site_setting :ghostmode_enabled
@@ -16,11 +16,11 @@ after_initialize do
         result.where(
           #'((posts.id NOT IN (?) AND posts.reply_to_post_number NOT IN (?)) OR posts.user_id = ?)',
           #'((posts.id NOT IN (?) AND posts.reply_to_post_number NOT IN (?)) OR posts.user_id IN (SELECT u.id FROM users u WHERE u.admin = ? OR u.id = ?))', #0.0.28
-          '((posts.id NOT IN (?) AND posts.reply_to_post_number NOT IN (?)) OR posts.user_id IN (SELECT u.id FROM users u WHERE u.admin = ?))',
+          '((posts.id NOT IN (?) AND posts.reply_to_post_number NOT IN (?)) OR posts.user_id IN (SELECT u.id FROM users u WHERE u.admin = ?) AND u.id = ?)',
           SiteSetting.ghostmode_posts.split('|'),
           SiteSetting.ghostmode_posts.split('|'),
           true,
-          #@user&.id || 0 #0.0.28
+          @user&.id || 0
         )
       end
     end
